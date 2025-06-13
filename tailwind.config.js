c1d5dc562020bd66b71f58d7794560f91b8e55f4
athsara bimalka <athsarab@gmail.com>
2025-06-14 01:49:54 +0530

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ export default {
         accent: 'rgb(var(--color-accent) / <alpha-value>)',
         background: 'rgb(var(--color-background) / <alpha-value>)',
       },
+      animation: {
+        bounce: 'bounce 6s infinite',
+        pulse: 'pulse 3s infinite',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
     },
   },
+  
   plugins: [],
 };
